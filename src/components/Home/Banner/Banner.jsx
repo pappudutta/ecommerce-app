03a15ed21ee9af40402de7b25c33800c56a332e4
Pappu Dutta <pappudutta279@gmail.com>
2023-08-31 12:45:38 +0530

    feat: Add banner image and content to home page

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,26 @@
 import "./Banner.scss";
+import BannerImage from "../../../assets/banner-img.png";
+
 const Banner = () => {
-    return <div>Banner</div>;
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>Sales</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore eum
+            repudiandae iusto! Sint eligendi impedit molestiae voluptatem atque
+            quidem! Placeat?
+          </p>
+          <div className="ctas">
+            <div className="baner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
+        </div>
+        <img className="banner-image" src={BannerImage} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
